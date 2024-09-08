297dff224738653fcb7e9c633583bff2b6e29063
waromero <contact@waromero.com>
2024-09-08 19:40:05 +0200

    Add notebooks.

--- lsseg_slicer_extension/lsseg_slicer_extension/lsseg/geometry.py
+++ lsseg_slicer_extension/lsseg_slicer_extension/lsseg/geometry.py
@@ -1,8 +1,343 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 # Name        : geometry.py
 # Description : The computational geometry functions module.
 # Authors     : William A. Romero R.     <william.romero@umontpellier.fr>
 #                                        <contact@waromero.com>
-#               Susana Uribe Vel squez   <susana.uribe7@eia.edu.co>
-#               Daniel Restrepo Qui ones <daniel.restrepo53@eia.edu.co>
+#               Susana Uribe Velásquez   <susana.uribe7@eia.edu.co>
+#               Daniel Restrepo Quiñones <daniel.restrepo53@eia.edu.co>
 #-------------------------------------------------------------------------------
+import scipy.optimize
+import SimpleITK as sitk
+import numpy as np
+
+
+class GeometryException(Exception):
+    """
+    Default exception from segmentation execution.
+    """
+    pass
+
+
+class CobbAngleCalculator(object):
+    """
+    Calculate the Cobb angle between two vertebrae.
+    """
+    def __init__( self, 
+                  image:sitk.Image,
+                  label_map:sitk.Image,
+                  uv_label_index = 0, 
+                  lv_label_index = 0 ):
+        """
+        Default constructor
+        """
+        self.description = "Cobb Angle Calculator Class."
+        self.image = image
+        self.label_map = label_map
+        self.lv_label_index = lv_label_index
+        self.uv_label_index = uv_label_index
+
+        self.dimension = image.GetDimension()
+
+        self.uv_oriented_bbox_vertices = None
+        self.lv_oriented_bbox_vertices = None
+
+        self.uv_p1 = None
+        self.uv_p2 = None
+        self.lv_p1 = None
+        self.lv_p2 = None
+
+        self.intersection_point = None
+
+        self.cobb_angle = 0
+
+
+    def __str__(self):
+        """
+        Default human-readable description of the class.
+        """
+        output_str = "\n{description}\n\n".format(description = self.description)
+        output_str += "\tImage dimension: {}\n".format(self.dimension)         
+        output_str += "\tLower vertebra label index: {}\n".format(self.lv_label_index) 
+        output_str += "\tUpper vertebra label index: {}\n\n".format(self.uv_label_index) 
+
+        if self.image is not None:
+            output_str += str(self.image)
+
+        if self.label_map is not None:
+            output_str += str(self.label_map)
+
+        return output_str
+
+
+    def __calc_uv_bbox_vertices(self):
+        """
+        Calculate UV oriented bounding box vertices.
+        """
+        lshape_stats_filter = sitk.LabelShapeStatisticsImageFilter()
+        lshape_stats_filter.SetBackgroundValue(0)       
+        lshape_stats_filter.ComputeOrientedBoundingBoxOn()
+        lshape_stats_filter.Execute(self.label_map)
+        
+        bounding_box_vertices = lshape_stats_filter.GetOrientedBoundingBoxVertices(self.uv_label_index)
+
+        # bbox_direction_vect = lshape_stats_filter.GetOrientedBoundingBoxDirection(self.uv_label_index)
+        
+        # bbox_direction_mat = [ bbox_direction_vect[0], bbox_direction_vect[3], bbox_direction_vect[6],
+        #                        bbox_direction_vect[1], bbox_direction_vect[4], bbox_direction_vect[7],
+        #                        bbox_direction_vect[2], bbox_direction_vect[5], bbox_direction_vect[8] ]
+
+
+        if( len(bounding_box_vertices) == 24 ):            
+            self.uv_oriented_bbox_vertices = np.array(bounding_box_vertices).reshape(8,3)
+
+            # #___DEBUG___
+            # uv_oriented_bbox_vertices = np.array(bounding_box_vertices).reshape(8,3)            
+            # T = sitk.Similarity3DTransform()
+            # T.SetMatrix(bbox_direction_mat)
+            # T.SetCenter(lshape_stats_filter.GetOrientedBoundingBoxOrigin(self.uv_label_index))
+
+            # self.uv_oriented_bbox_vertices = np.zeros_like(uv_oriented_bbox_vertices)
+            # for i, j in enumerate(uv_oriented_bbox_vertices):
+            #     self.uv_oriented_bbox_vertices[i] = T.TransformPoint(j)
+            # #___END___
+            
+        if( len(bounding_box_vertices) == 8 ):
+            self.uv_oriented_bbox_vertices = np.array(bounding_box_vertices).reshape(4,2)
+
+ 
+    def __calc_lv_bbox_vertices(self):
+        """
+        Calculate LV oriented bounding box vertices.
+        """
+        lshape_stats_filter = sitk.LabelShapeStatisticsImageFilter()
+        lshape_stats_filter.ComputeOrientedBoundingBoxOn()
+        lshape_stats_filter.SetBackgroundValue(0)
+        lshape_stats_filter.Execute(self.label_map)
+        
+        bounding_box_vertices = lshape_stats_filter.GetOrientedBoundingBoxVertices(self.lv_label_index)
+
+        # bbox_direction_vect = lshape_stats_filter.GetOrientedBoundingBoxDirection(self.lv_label_index)
+        
+        # bbox_direction_mat = [ bbox_direction_vect[0], bbox_direction_vect[3], bbox_direction_vect[6],
+        #                        bbox_direction_vect[1], bbox_direction_vect[4], bbox_direction_vect[7],
+        #                        bbox_direction_vect[2], bbox_direction_vect[5], bbox_direction_vect[8] ]
+        
+        if( len(bounding_box_vertices) == 24 ): 
+            self.lv_oriented_bbox_vertices = np.array(bounding_box_vertices).reshape(8,3)
+
+            # #___DEBUG___
+            # lv_oriented_bbox_vertices = np.array(bounding_box_vertices).reshape(8,3)
+            # T = sitk.Similarity3DTransform()
+            # T.SetMatrix(bbox_direction_mat)
+            # T.SetCenter(lshape_stats_filter.GetOrientedBoundingBoxOrigin(self.lv_label_index))
+
+            # self.lv_oriented_bbox_vertices = np.zeros_like(lv_oriented_bbox_vertices)
+            # for i, j in enumerate(lv_oriented_bbox_vertices):
+            #     self.lv_oriented_bbox_vertices[i] = T.TransformPoint(j)   
+            # #___DEBUG___    
+            
+        if( len(bounding_box_vertices) == 8 ):
+            self.lv_oriented_bbox_vertices = np.array(bounding_box_vertices).reshape(4,2)
+
+
+    def __set_bbox_tan_line_points(self):
+        """
+        Set the two points for the tangent  line to the upper  plane /
+        upper vertebra  bounding box  and two  points for  the tangent
+        line to the lower plane / lower vertebra bounding box.
+        """
+        if( self.uv_oriented_bbox_vertices.size == 24 ):
+            
+            self.uv_p1 = [ self.uv_oriented_bbox_vertices[0,0],
+                           self.uv_oriented_bbox_vertices[0,1],
+                           self.uv_oriented_bbox_vertices[0,2] ]
+
+            self.uv_p2 = [ self.uv_oriented_bbox_vertices[1,0], 
+                           self.uv_oriented_bbox_vertices[1,1], 
+                           self.uv_oriented_bbox_vertices[1,2] ]
+
+            self.lv_p1 = [ self.lv_oriented_bbox_vertices[6,0], 
+                           self.lv_oriented_bbox_vertices[6,1], 
+                           self.lv_oriented_bbox_vertices[6,2] ]
+
+            self.lv_p2 = [ self.lv_oriented_bbox_vertices[7,0], 
+                           self.lv_oriented_bbox_vertices[7,1], 
+                           self.lv_oriented_bbox_vertices[7,2] ]
+
+            return self.uv_p1, self.uv_p2, self.lv_p1, self.lv_p2
+
+
+        if( self.uv_oriented_bbox_vertices.size == 8 ):
+            
+            self.uv_p1 = [ self.uv_oriented_bbox_vertices[0,0],
+                           self.uv_oriented_bbox_vertices[0,1] ]
+
+            self.uv_p2 = [ self.uv_oriented_bbox_vertices[1,0], 
+                           self.uv_oriented_bbox_vertices[1,1] ]
+
+            self.lv_p1 = [ self.lv_oriented_bbox_vertices[2,0], 
+                           self.lv_oriented_bbox_vertices[2,1] ]
+
+            self.lv_p2 = [ self.lv_oriented_bbox_vertices[3,0], 
+                           self.lv_oriented_bbox_vertices[3,1] ]
+        
+
+            return self.uv_p1, self.uv_p2, self.lv_p1, self.lv_p2
+
+
+    def __calc_angle_between_lines(self):
+        """
+        Calculates the angle between two lines:
+            line1 = { (p1x, p1y, p1z), (p2x, p2y, p2z) }
+            line2 = { (q1x, q1y, q1z), (q2x, q2y, q2z) }    
+        """
+        p1 = np.array(self.uv_p1)
+        p2 = np.array(self.uv_p2)
+        q1 = np.array(self.lv_p1)
+        q2 = np.array(self.lv_p2)
+
+        # Direction vectors.
+        line1 = p2 - p1
+        line2 = q2 - q1
+
+        dot_product = np.dot(line1, line2)
+
+        # Magnitudes (norms).
+        norm_line1 = np.linalg.norm(line1)
+        norm_line2 = np.linalg.norm(line2)
+
+        cos_theta = dot_product / (norm_line1 * norm_line2)
+
+        cos_theta = np.clip(cos_theta, -1.0, 1.0)
+
+        theta_rad = np.arccos(cos_theta)
+
+        # Convert the angle to degrees
+        theta_deg = np.degrees(theta_rad)
+
+        return theta_deg
+
+    
+    def __calc_intersection(self):
+        """
+        Calculate the intersection point between:
+        (p1p2) and (q1q2).
+        """
+        p1 = np.array(self.uv_p1)
+        p2 = np.array(self.uv_p2)
+        q1 = np.array(self.lv_p1)
+        q2 = np.array(self.lv_p2)
+
+        def errFunc(estimates):
+            s, t = estimates
+            x = p1 + s * (p2 - p1) - (q1 + t * (q2 - q1))
+            return x
+
+        estimates = [1, 1]
+
+        sols = scipy.optimize.least_squares(errFunc, estimates)
+        s,t = sols.x
+
+        x1 = p1[0] + s * (p2[0] - p1[0])
+        x2 = q1[0] + t * (q2[0] - q1[0])
+        y1 = p1[1] + s * (p2[1] - p1[1])
+        y2 = q1[1] + t * (q2[1] - q1[1])
+        z1 = p1[2] + s * (p2[2] - p1[2])
+        z2 = q1[2] + t * (q2[2] - q1[2])
+
+        x = (x1 + x2) / 2  
+        y = (y1 + y2) / 2  
+        z = (z1 + z2) / 2  
+
+        self.intersection_point = [x, y, z]
+
+
+    def execute(self):
+        """
+        Main public method for executing the method.
+        """
+        self.__calc_uv_bbox_vertices()
+        self.__calc_lv_bbox_vertices()
+
+        self.__set_bbox_tan_line_points()
+
+        self.cobb_angle = self.__calc_angle_between_lines()
+
+        self.__calc_intersection()
+
+        
+    def get_uv_oriented_bbox_vertices(self):
+        """
+        Return upper vertebra oriented bounding box vertices.
+        """
+        return self.uv_oriented_bbox_vertices
+
+
+    def get_lv_oriented_bbox_vertices(self):
+        """
+        Return lower vertebra oriented bounding box vertices.
+        """
+        return self.lv_oriented_bbox_vertices
+
+
+    def get_uv_oriented_bbox_indices(self):
+        """
+        Return upper vertebra oriented bounding box vertices.
+        """
+        vertices_img_indices = np.zeros_like(self.uv_oriented_bbox_vertices)
+        for i, j in enumerate(self.uv_oriented_bbox_vertices):
+            vertices_img_indices[i] = self.label_map.TransformPhysicalPointToIndex(j)
+
+        return vertices_img_indices
+
+
+    def get_lv_oriented_bbox_indices(self):
+        """
+        Return lower vertebra oriented bounding box vertices.
+        """
+        vertices_img_indices = np.zeros_like(self.lv_oriented_bbox_vertices)
+        for i, j in enumerate(self.lv_oriented_bbox_vertices):
+            vertices_img_indices[i] = self.label_map.TransformPhysicalPointToIndex(j)
+
+        return vertices_img_indices
+    
+    
+    def get_cobb_angle(self):
+        """
+        Return the value of the Cobb angle.
+        """
+        return self.cobb_angle    
+
+
+    def get_intersection_point_index(self):
+        """
+        Return the intersection point between uv and lv.
+        """
+        intersection_point = self.image.TransformPhysicalPointToIndex((self.intersection_point[0],
+                                                                       self.intersection_point[1],
+                                                                       self.intersection_point[2]))
+
+        return intersection_point
+
+    
+    def get_angle_indices(self):
+        """
+        Return the 3-Point of the Cobb angle.
+        """
+        ca_p1 = self.image.TransformPhysicalPointToIndex((self.uv_p1[0],
+                                                          self.uv_p1[1],
+                                                          self.uv_p1[2]))
+
+        ca_p2 = self.image.TransformPhysicalPointToIndex((self.intersection_point[0],
+                                                          self.intersection_point[1],
+                                                          self.intersection_point[2]))        
+        
+        ca_p3 = self.image.TransformPhysicalPointToIndex((self.lv_p1[0],
+                                                          self.lv_p1[1],
+                                                          self.lv_p1[2]))
+
+
+        return ca_p1, ca_p2, ca_p3
+
--- lsseg_slicer_extension/lsseg_slicer_extension/lsseg/segmentation.py
+++ lsseg_slicer_extension/lsseg_slicer_extension/lsseg/segmentation.py
@@ -8,3 +8,416 @@
 #               Susana Uribe Velásquez   <susana.uribe7@eia.edu.co>
 #               Daniel Restrepo Quiñones <daniel.restrepo53@eia.edu.co>
 #-------------------------------------------------------------------------------
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import SimpleITK as sitk
+import numpy as np
+
+
+def get_2D_image_from( image_3d, 
+                         method='SLICE', 
+                         slice_index=None, axis=2 ):
+    """
+    Return a 2D image from a 3D image using:
+
+        - SLICE index, 
+        - Max. Intensity Projection (MIP), or 
+        - Mean Projection (MP)
+
+    <axis> defines the axis along which to project:
+        - 0=X, 1=Y and  2=Z (within MIP/MP).
+    """
+    if method == "SLICE":
+        if slice_index is None:
+            raise ValueError("slice_index must be provided for 'slice' method.")
+
+        return image_3d[:, :, slice_index]
+    
+    elif method == "MIP":
+        return sitk.MaximumProjection(image_3d, projectionDimension=axis)
+    
+    elif method == "MP":
+        return sitk.MeanProjection(image_3d, projectionDimension=axis)
+    
+    else:
+        raise ValueError("Unsupported method. Use 'SLICE', 'MIP', or 'MP'.")
+
+
+class SubImageManager2DException(Exception):
+    """
+    Default exception from SubImageManager2D methods.
+    """
+    pass
+
+
+class SubImageManager2D(object):
+    """
+    Threshold-Based Segmentation.
+    """
+    def __init__( self,
+                  image:sitk.Image,
+                  centre_col:int, 
+                  centre_row:int,
+                  width:int, 
+                  height:int ):
+        """
+        Default constructor
+        """
+        self.description = "Sub-Image Manager (2D)"
+        self.image = image
+        self.centre_col = centre_col
+        self.centre_row = centre_row
+        self.width = width
+        self.height = height
+
+        self.start_col = 0
+        self.start_row = 0
+
+        self.end_col = 0
+        self.end_row = 0        
+
+        self.calculate_indices()
+
+
+    def __str__(self):
+        """
+        Default human-readable description of the class.
+        """
+        output_str = "\n{description}\n\n".format(description = self.description)
+        output_str += "\tCentre Column: {}\n".format(self.centre_col) 
+        output_str += "\tCentre Row: {}\n".format(self.centre_row) 
+        output_str += "\tWidth: {}\n".format(self.width) 
+        output_str += "\tHeight: {}\n\n".format(self.height) 
+
+        if self.image is not None:
+            output_str += str(self.image)
+
+        return output_str
+
+
+    def calculate_indices(self):
+        """
+        Extract a region of interest from the image.    
+        """        
+        self.start_col = int(self.centre_col - self.width // 2)
+        self.start_row = int(self.centre_row - self.height // 2)
+        
+        image_size = self.image.GetSize()
+        image_width = image_size[0]
+        image_height = image_size[1]
+        
+        self.start_col = max(0, min(self.start_col, image_width - 1))
+        self.start_row = max(0, min(self.start_row, image_height - 1))
+        
+        self.end_col = min(self.start_col + self.width, image_width)
+        self.end_row = min(self.start_row + self.height, image_height)
+
+
+    def extract_roi( self ) -> sitk.Image:
+        """
+        Extract a region of interest from the image.    
+        """ 
+        try:
+            roi_size = [self.end_col - self.start_col, self.end_row - self.start_row]
+            roi_start_index = [self.start_col, self.start_row]
+
+            roi = sitk.RegionOfInterest( self.image, 
+                                         size = roi_size, 
+                                         index = roi_start_index )
+            
+        except Exception as exception:
+            print("[SubImageManager2D::Execution Exception] %s" % str(exception))
+            raise SubImageManager2DException(str(exception))            
+
+        return roi
+    
+
+    def get_empty_image_with(self, roi) -> sitk.Image:
+        """
+        Insert a region of interest back into the original image space.
+        """
+        output_image = sitk.Image(self.image.GetSize(), self.image.GetPixelIDValue())
+        output_image.SetOrigin(self.image.GetOrigin())
+        output_image.SetSpacing(self.image.GetSpacing())
+        output_image.SetDirection(self.image.GetDirection())
+
+        # Iterate through the ROI and place it into the original image
+        for row in range(self.start_row, self.end_row):
+            for col in range(self.start_col, self.end_col):
+                output_image.SetPixel(col, row, roi.GetPixel(col - self.start_col, row - self.start_row))
+        
+        return sitk.Cast(output_image, sitk.sitkUInt8)        
+
+
+@dataclass
+class SegmentationModel:
+    """
+    Segmentation.
+    """
+    index: int = -1
+    name: str = "None"
+    description: str = "None"
+    file_path: str = "None"
+
+
+class SegmentationException(Exception):
+    """
+    Default exception from segmentation execution.
+    """
+    pass
+
+
+class Segmentation(ABC):
+    """
+    Segmentation Abstract Class.
+    """
+    @abstractmethod
+    def fnc(self):
+        """
+        Default fnc method.
+        """
+        pass
+
+
+    @abstractmethod
+    def execute(self):
+        """
+        Execute segmentation.
+        """
+        pass
+
+
+class ThresholdSegmentation(Segmentation):
+    """
+    Threshold-Based Segmentation.
+    """
+    def __init__( self, 
+                   image:sitk.Image, 
+                   lb = 0, 
+                   ub = 0 ):
+        """
+        Default constructor
+        """
+        self.description = "Threshold-Based segmentation."
+        self.image = image
+        self.lower_threshold = lb
+        self.upper_threshold = ub
+
+        self.__automatic_thresholds = False
+
+        self.__bins = 27
+        self.__lbp = 0.46
+        self.__ubp = 0.81
+
+
+    def __str__(self):
+        """
+        Default human-readable description of the class.
+        """
+        output_str = "\n{description}\n\n".format(description = self.description)
+        output_str += "\tLower-Threshold: {}\n".format(self.lower_threshold) 
+        output_str += "\tUpper-Threshold: {}\n\n".format(self.upper_threshold) 
+
+        output_str += "\tHistogram bins: {}\n".format(self.__bins)
+        output_str += "\tLower-Bound percentage: {}\n".format(self.__lbp)
+        output_str += "\tUpper-Bound percentage: {}\n\n".format(self.__ubp)
+
+        
+        if self.image is not None:
+            output_str += str(self.image)
+
+        return output_str
+
+
+    @property
+    def hist_bins(self):
+        """
+        Histogram bins.
+        """
+        return self.__bins
+  
+    
+    @property
+    def sigma_lbp(self):
+        """
+        Set the lower-bound percentage.
+        """
+        return self.__lbp
+
+    
+    @property
+    def sigma_ubp(self):
+        """
+        Set the lower-bound percentage.
+        """
+        return self.__ubp
+        
+    
+    @hist_bins.setter
+    def hist_bins(self, bins):
+        """
+        Set the number of bins.
+        """
+        self.__bins = bins
+
+
+    @sigma_lbp.setter
+    def sigma_lbp(self, perc):
+        """
+        Set the lower-bound percentage.
+        """
+        self.__lbp = perc
+
+        
+    @sigma_ubp.setter
+    def sigma_ubp(self, perc):
+        """
+        Set the upper-bound percentage.
+        """
+        self.__ubp = perc
+        
+
+    def set_automatic_thresholds_ON(self):
+        """
+        Set automatic calculation of the lower and upper thresholds.
+        """
+        self.__automatic_thresholds = True
+
+
+    def fnc(self):
+        """
+        Not required
+        """
+        return 0
+
+
+    def calculate_thresholds(self):
+        """
+        Calculate lower and upper thresholds.
+        """
+        hthreshold_filter = sitk.HuangThresholdImageFilter()
+        hthreshold_filter.SetNumberOfHistogramBins(self.__bins)
+        hthreshold_filter.SetInsideValue(0)
+        hthreshold_filter.SetOutsideValue(1)
+
+        hthreshold_output = hthreshold_filter.Execute(self.image)
+
+        stats_filter = sitk.LabelStatisticsImageFilter()
+        stats_filter.Execute(self.image, hthreshold_output)
+
+        mean =  stats_filter.GetMean(1)
+        sigma = stats_filter.GetSigma(1)
+        
+        self.lower_threshold = np.floor(mean - (sigma*self.__lbp))
+        self.upper_threshold = np.ceil(mean + (sigma*self.__ubp))
+
+
+    def execute(self):
+        """
+        Execute segmentation.
+        """
+        try:
+
+            if self.__automatic_thresholds:
+                self.calculate_thresholds()
+
+            threshold_filter = sitk.BinaryThresholdImageFilter()
+            threshold_filter.SetLowerThreshold(self.lower_threshold)
+            threshold_filter.SetUpperThreshold(self.upper_threshold)
+            threshold_filter.SetOutsideValue(0)
+            threshold_filter.SetInsideValue(1)
+            threshold_output = threshold_filter.Execute(self.image)
+
+            connected_components_filter = sitk.ConnectedComponentImageFilter()
+            connected_components_filter.SetFullyConnected(False)
+            connected_components = connected_components_filter.Execute(threshold_output)
+
+            labels_stats_filters = sitk.LabelShapeStatisticsImageFilter()
+            labels_stats_filters.Execute(connected_components)
+
+            components = labels_stats_filters.GetLabels()
+
+            max_size = 0
+            max_label_index = 1
+
+            for label_index in components:
+                current_size = labels_stats_filters.GetNumberOfPixels(label_index)
+
+                if (current_size > max_size) :
+                        max_size = current_size
+                        max_label_index = label_index
+
+
+            threshold_filter = sitk.BinaryThresholdImageFilter()
+            threshold_filter.SetLowerThreshold(max_label_index)
+            threshold_filter.SetUpperThreshold(max_label_index)
+            threshold_filter.SetOutsideValue(0)
+            threshold_filter.SetInsideValue(1)
+            max_connected_component = threshold_filter.Execute(connected_components)
+
+            fill_filter = sitk.BinaryFillholeImageFilter()
+            
+            output_segmentation = fill_filter.Execute(max_connected_component)
+
+            return output_segmentation
+
+        except Exception as exception:
+            print("[ThresholdSegmentation::Execution Exception] %s" % str(exception))
+            raise SegmentationException(str(exception))
+
+
+class ModelBasedSegmentation(Segmentation):
+    """
+    Threshold-Based Segmentation.
+    """
+    def __init__( self, 
+                  inputImage:sitk.Image, 
+                  inputModel:SegmentationModel ) -> sitk.Image:
+        """
+        Default constructor
+        """
+        self.description = "Model-Based segmentation."
+        self.image = inputImage
+        self.model = inputModel
+
+        self.__loadModel
+
+
+    def __str__(self):
+        """
+        Default human-readable description of the class.
+        """
+        output_str = "\n{description}\n\n".format(description = self.description)
+
+        if self.image is not None:
+            output_str += str(self.image)
+
+        return output_str
+
+
+    def fnc(self):
+        """
+        Not required
+        """
+        return 0
+
+
+    def execute(self):
+        """
+        Execute segmentation.
+        """
+        try:
+            #---------------------------
+            # TODO: Segmentation.
+            #---------------------------
+
+            segmentation_output = None
+
+            return segmentation_output
+
+        except Exception as exception:
+            print("[ModelBasedSegmentation::Execution Exception] %s" % str(exception))
+            raise SegmentationException(str(exception))
+
+
